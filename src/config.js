09d77ee6cfe31651f2fc6fb120f7be20ead047b4
Ryan Jeske <ryanjeske@gmail.com>
2019-05-10 16:02:33 -0600

    configured for prod build

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_ENDPOINT: `https://still-woodland-32378.herokuapp.com/`,
   API_KEY: process.env.REACT_APP_API_KEY
 };
